428263ae7bfb8170545db12918a6e8aaa0e2cb28
cesarca28 <12348@holbertonstudents.com>
2025-10-23 14:07:09 +0200

    commit

---
+++ hello_world/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Imprime la valeur de 'n'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main()
+{
+	int n = 15;
+	int A = 1;
+	n = n + A;
+	printf("La valeur de n est : %d\net de A : %d\n", n, A);
+	return(0);
+}

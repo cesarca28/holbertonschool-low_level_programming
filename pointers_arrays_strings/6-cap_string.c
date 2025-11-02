7f034533a8c45821b4b5a8b99928a67ed7169719
cesarca28 <12348@holbertonstudents.com>
2025-11-02 16:59:59 +0100

    commit push

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -30,4 +30,24 @@ char *cap_string(char *str)
 	int i = 0;
 	int capitalize_next = 1;
 
-	while (str[i] != '\0'
+	while (str[i] != '\0')
+	{
+		if (capitalize_next && str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+
+		if (is_separator(str[i]))
+		{
+			capitalize_next = 1;
+		}
+		else
+		{
+			capitalize_next = 0;
+		}
+
+		i++;
+	}
+
+	return (str);
+}

45439c26a967e471ca39e4ce9c17adc2ace59616
cesarca28 <12348@holbertonstudents.com>
2025-11-03 14:30:56 +0100

    commit 1-main_mempcy.c

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -8,12 +8,12 @@
 *
 *Return: pointer to dest
 */
-char *_memcpy(char *dest, char *src, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
 
 	for (i = 0; i < n; i++)
-	dest[i] = src;
+	dest[i] = src[i];
 
 	return (dest);
 
